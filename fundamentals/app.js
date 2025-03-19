f242cfb0140acb1de73498e2ea7b6c17ed98dd80
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-03-19 16:16:28 +0600

    Basic JavaScript

---
+++ fundamentals/app.js
@@ -0,0 +1,97 @@
+/*
+console.log("Hello World");
+
+let js = "amazing";
+ console.log(6
+3 + 367 + 23 - 23);
+*/
+
+/*
+Multiline comments
+Thank You Javascript
+You're welcome devnuru
+
+*/
+
+//console.log(js);
+/*
+console.log("Md. Nuruzzaman");
+console.log(44);
+
+let firstName = "Devnuru";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// Variable name conventions
+letmyFirstJob = "Developer";
+let myCurrentJob = "Programmer";
+
+let job1 = "Developer";
+let job2 = "Programmer";
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+// dynamic typing - javascript is dynamically typed language
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+// let, const, var
+// let is block-scoped
+// var is function-scoped
+// const is block-scoped and cannot be reassigned
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+// birthYear = 1998; // error
+
+var job = 'programmer';
+job = 'developer';
+
+lastName = 'Chowdhury';
+console.log(lastName);
+
+*/
+
+// Basic Operators
+// Math operators 
+  
+const now = 2025;
+const ageNuru = now - 1997;
+const ageWife = now - 2005;
+console.log(ageNuru, ageWife);
+
+console.log(ageNuru * 2, ageNuru / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Md. Nuruzzaman';
+const lastName = 'Chowdhury';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--;
+x--;
+console.log(x);
---
+++ fundamentals/assignments.js
@@ -0,0 +1,14 @@
+let country = "Bangladesh";
+let continent = "Asia";
+let population = 170;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);

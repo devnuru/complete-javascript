c65bda277a688df507440882529a58e32a48dce2
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-03-31 14:13:44 +0600

    strict mode

---
+++ fundamentals2/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// So again, first strict mode forbids us to do certain things
+
+//and second it creates visible errors
+
+//and the developer console, where in other situations
+
+// more sucure code
+
+
+let hasDiverslicense = false;
+const passTest = true;
+
+if (passTest) hasDiverslicense = true
+if(hasDiverslicense) console.log('I can drive :D');
+
+// const interface = 'Video'
+
+// n JavaScript, "use strict"; is a directive that enables strict mode, which helps catch common coding mistakes and improves security. It was introduced in ECMAScript 5 (ES5) and can be applied to an entire script or a specific function.

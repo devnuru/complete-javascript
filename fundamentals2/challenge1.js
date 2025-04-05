5cabfbcfa5b3c2cd5006b9e85c7aadd9e3a5f805
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-04-05 15:22:27 +0600

    challenge 4

--- fundamentals2/challenge1.js
+++ fundamentals2/challenge1.js
@@ -146,3 +146,34 @@ if (john.bmi > mark.bmi) {
 } else if(mark.bmi > john.bmi) {
      console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
 }
+
+
+
+// Challenge 4
+
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage(tips));
+
+

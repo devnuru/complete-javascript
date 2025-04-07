0a68f27b47d3d56699c3631b120658e4a8311d67
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-04-07 11:46:58 +0600

    DOM Manipulation

---
+++ guess-number/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const msg = document.querySelector('.message');
+
+console.log(msg.textContent);
+
+document.querySelector('.message').textContent = '🦀Correct Number!';
+
+console.log(msg.textContent);
+
+// What is DOM?
+// DOM is Document Object Model
+// DOM is a programming interface for web documents
+// DOM represents the structure of a document as a tree of objects
+// DOM allows programming languages to manipulate the structure, style, and content of web documents
+// DOM is a language-neutral interface
+// DOM is a platform-neutral interface
+// DOM is a standard interface
+// DOM is a W3C standard
+// DOM is a W3C recommendation
+// DOM is a W3C specification
+
+// So we can say that DOM is basically a connection point between HTML documents and JavaScript code.
+
+// DOM !== JAVASCRIPT;
+
+// DOM methods and Properties for DOM Manipulation
+// DOM is not Part of JavaScript
+
+// DOM is part of Web APIs
+// the DOM and DOM methods are actually part of something called the web APIs.
+
+// Web APIs are provided by the browser
+
+
+document. querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 22;
+console.log(document.querySelector('.guess').value);
+

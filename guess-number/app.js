3f12b3ae81970cba81e6661f2f5698011d6b74ca
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-04-08 17:13:53 +0600

    Guess the secret number

--- guess-number/app.js
+++ guess-number/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const msg = document.querySelector('.message');
+// const msg = document.querySelector('.message');
 
-console.log(msg.textContent);
+// console.log(msg.textContent);
 
-document.querySelector('.message').textContent = '🦀Correct Number!';
+// document.querySelector('.message').textContent = '🦀Correct Number!';
 
-console.log(msg.textContent);
+// console.log(msg.textContent);
 
 // What is DOM?
 // DOM is Document Object Model
@@ -32,10 +32,70 @@ console.log(msg.textContent);
 
 // Web APIs are provided by the browser
 
+// document. querySelector('.number').textContent = 15;
+// document.querySelector('.score').textContent = 10;
 
-document. querySelector('.number').textContent = 15;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 22;
+// console.log(document.querySelector('.guess').value);
 
-document.querySelector('.guess').value = 22;
-console.log(document.querySelector('.guess').value);
+// Event Handling
 
+// Event Handling is a way to execute a function when an event occurs
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    // When Player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✅ Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem'
+
+    // When guess too High
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 To High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😾 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 To Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😾 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').style.width = '15rem';
+
+})

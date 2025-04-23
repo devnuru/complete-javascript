71dbd82eef0fc626bf8bfe478c3545fec4b987e7
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-04-23 16:24:32 +0600

    how passing arguments work

--- 6.functions/app.js
+++ 6.functions/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookingsArr = [];
 
 const createBooking = function (flightName, numPassengers = 1, price = 190 * numPassengers) {
@@ -22,3 +23,46 @@ createBooking('LH1234', 2, 800);
 createBooking('LH1234', 2);
 createBooking('LH1234', 8);
 createBooking('LH1234', undefined, 800);
+
+
+
+*/
+
+const flight = 'LH234';
+const devnuru = {
+  name: 'Md. Nuruzzaman Chowdhury',
+  passport: 73478237893,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH734';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 73478237893) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, devnuru);
+// console.log(flight);
+// console.log(devnuru);
+
+// // Is the same as doing ....
+// const flightNum = flight;
+// const passenger = devnuru;
+
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+
+}
+newPassport(devnuru);
+checkIn(flight, devnuru)
+
+
+// JavaScript does not have passing by reference, only passing by value, even though it looks like it's passing by reference.
+
+
+

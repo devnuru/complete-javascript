401b643cb0177cb6b468c2defa00bb0bc6de6da0
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-09-13 22:06:40 +0600

    The Filter method

--- 7.arrays-bankist/app.js
+++ 7.arrays-bankist/app.js
@@ -215,3 +215,20 @@ const movementsDescriptions = movements.map((mov, i) =>
 
 console.log(movementsDescriptions);
 
+
+/// Filter Method
+const deposits = movements.filter(function(mov, i, arr) {
+return mov > 0;
+})
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = []
+for(const mov of movements) if(mov > 0) depositsFor.push(mov)
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => 
+  mov < 0
+)
+console.log(withdrawals);
+
